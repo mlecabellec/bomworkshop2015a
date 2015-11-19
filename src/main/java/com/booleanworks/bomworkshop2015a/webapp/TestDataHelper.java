0b2040ef2f27e2a2a801f51febc3551f4c5bfd4e
vortigern <mickael.lecabellec@gmail.com>
2015-11-19 23:17:56 +0100

    +quartz config fix +Test data

--- src/main/java/com/booleanworks/bomworkshop2015a/webapp/TestDataHelper.java
+++ src/main/java/com/booleanworks/bomworkshop2015a/webapp/TestDataHelper.java
@@ -8,7 +8,11 @@ package com.booleanworks.bomworkshop2015a.webapp;
 import com.booleanworks.bomworkshop2015a.entity.oagi10.bom.v1.SyncBOMType;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.xml.bind.JAXB;
@@ -19,6 +23,7 @@ import javax.xml.bind.JAXB;
  * @author vortigern
  */
 public class TestDataHelper implements ServletContextListener {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.booleanworks_bomworkshop2015a_war_1.0-SNAPSHOTPU");
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -36,6 +41,9 @@ public class TestDataHelper implements ServletContextListener {
             if (testsyncBom != null) {
                 Logger.getGlobal().warning("TestDataHelper.contextInitialized, got an object !!!");
                 System.out.println("TestDataHelper.contextInitialized, got an object !!!");
+                
+                emf.createEntityManager().persist(testsyncBom);
+                
             }else
             {
                 Logger.getGlobal().warning("TestDataHelper.contextInitialized, NO object !!!");
@@ -56,6 +64,20 @@ public class TestDataHelper implements ServletContextListener {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         Logger.getGlobal().warning("TestDataHelper.contextDestroyed");
         System.out.println("TestDataHelper.contextDestroyed");
+    }
+
+    public void persist(Object object) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
     }
 
 }
